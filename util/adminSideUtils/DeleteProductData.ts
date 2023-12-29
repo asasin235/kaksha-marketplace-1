444f4eec059617c357a92463972138d57720035a
ashishchaudhary66 <ashishkumarchaudhary66@gmail.com>
2023-12-29 22:31:35 +0530

    delete handler

---
+++ util/adminSideUtils/DeleteProductData.ts
@@ -0,0 +1,14 @@
+const Product=require(".../models/Product");
+
+exports.DeleteProduct=async(req,res)=>{
+    try{
+        const {id}=req.body;
+        const product=await Product.findByIdAndUpdate(id,{isDeleted:true})
+        res.status(200).json({
+            message:"Deleted Successfully"
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+}
