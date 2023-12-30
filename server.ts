1e73955e7551526b7be7437f9deeb3004023eda4
X0sphere <118646588+X0sphere@users.noreply.github.com>
2023-12-30 15:41:03 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const router = express.Router();
 const port = 3000;
+const admin_routes = require("./routes/admin")
+
 // TODO: Connect DB
 router.get('/', (req, res) => {
   res.send('Hello, world!');
@@ -11,7 +13,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
+app.use("/api/admin",admin_routes);
 /*
 // Path: server.ts
 read_user
