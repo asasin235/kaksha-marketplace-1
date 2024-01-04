9fb1f3c5df9be5646146fd33bd877dc9a9227a1d
Binay6 <88644022+Binay6@users.noreply.github.com>
2024-01-04 12:19:51 +0530

    Update server.ts connecting mongoDb

--- server.ts
+++ server.ts
@@ -1,15 +1,27 @@
-const express = require('express')
+const express=require('express');
 const app = express();
-const router = express.Router();
+const router=express.Router()
+const mongoose = require('mongoose') ;
 const port = 3000;
 // TODO: Connect DB
-router.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+const url="url with username pass" ;
+mongoose.connect(url).then((res)=>console.log("db connected"));
+  
+
+router.get('/',(req,res)=>{
+  
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+/*
+// Path: server.ts
+read_user
+kaksha-live
+ */
 
 
 /*
