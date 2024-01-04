483bcb1004bc7d5fa2e46f664b3fd4f21798faf9
Ashish Kumar Chaudhary <121446507+ashishchaudhary66@users.noreply.github.com>
2024-01-04 06:04:28 +0530

    Update id to product id in  DeleteProductData.ts

--- util/adminSideUtils/DeleteProductData.ts
+++ util/adminSideUtils/DeleteProductData.ts
@@ -2,8 +2,8 @@ const Product=require(".../models/Product");
 
 exports.DeleteProductData=async(req,res)=>{
     try{
-        const {id}=req.body;
-        const product=await Product.findByIdAndUpdate(id,{isDeleted:true})
+        const {productId}=req.body;
+        const product=await Product.findByIdAndUpdate({productId:productId},{isDeleted:true})
         res.status(200).json({
             message:"Deleted Successfully"
         })
