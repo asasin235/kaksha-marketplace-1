2532168e853dc07001b733d30576c44adba2b07e
shania524 <parweenshania@gmail.com>
2024-01-13 16:53:21 +0530

    category file added

--- models/products.ts
+++ models/products.ts
@@ -1,76 +1,69 @@
+// product.model.ts
 
 import mongoose, { Schema, model, Document } from "mongoose";
+import AutoIncrement from "mongoose-sequence";
 
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+import { ICategory } from "./category";
 
-export interface IProduct {
+interface IOtherImageUrl {
+    url: string;
+    description?: string;
+}
+
+export interface IProduct extends Document {
     productId: number;
     productName: string;
     price: number;
-    category: ICategory; // Reference to Category interface
+    category: ICategory["_id"];
+    imageUrl: string;
+    otherImageUrl: IOtherImageUrl;
     metadata: object;
     classes: object;
     isDeleted: boolean;
     organization: string;
     description: string;
-    imageUrl: string; // Image URL of string type
-    otherImageUrl: {
-        url: string;
-        description?: string;
-    }; // Image URL of object type with optional description
 }
-
-export interface ICategory {
-    categoryId: number;
-    categoryName: string;
-}
-
-const categorySchema = new Schema<ICategory>(
-    {
-        categoryId: {
-            type: Number,
-            unique: true
-        },
-        categoryName: {
-            type: String,
-            required: true
-        },
-    },
-    {
-        timestamps: true,
-    }
-);
-
-categorySchema.plugin(AutoIncrement, { inc_field: 'categoryId' });
-
-export const Category = model<ICategory>("Category", categorySchema);
 
 const productSchema = new Schema<IProduct>(
     {
         productId: {
             type: Number,
-            unique: true
+            unique: true,
         },
         productName: {
             type: String,
-            required: true
+            required: true,
         },
         price: {
             type: Number,
-            required: true
+            required: true,
         },
         category: {
             type: Schema.Types.ObjectId,
-            ref: 'Category',
-            required: true
+            ref: "Category",
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+        otherImageUrl: {
+            url: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: false,
+            },
         },
         classes: {
             type: Object,
-            required: false
+            required: false,
         },
         metadata: {
             type: Object,
-            required: false
+            required: false,
         },
         isDeleted: {
             type: Boolean,
@@ -84,26 +77,12 @@ const productSchema = new Schema<IProduct>(
             type: String,
             required: false,
         },
-        imageUrl: {
-            type: String, // Image URL of string type
-            required: true,
-        },
-        otherImageUrl: {
-            url: {
-                type: String, // Image URL of object type
-                required: true,
-            },
-            description: {
-                type: String, // Optional description for the image URL
-                required: false,
-            },
-        },
     },
     {
         timestamps: true,
     }
 );
 
-productSchema.plugin(AutoIncrement, { inc_field: 'productId' });
+productSchema.plugin(AutoIncrement, { inc_field: "productId" });
 
 export const Product = model<IProduct>("Product", productSchema);
