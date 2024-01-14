e4579eb1c5cb4d80f2378a10267fe3aebc998969
Saurabh Sharma <118646588+X0sphere@users.noreply.github.com>
2024-01-14 16:53:13 +0530

    added clear-cart (#13)
    * added clear-cart
    
    * Update User.ts
    
    ---------
    
    Co-authored-by: Aatif Rashid <aatif20@gmail.com>

--- routes/user.ts
+++ routes/user.ts
@@ -1,8 +1,9 @@
 const express = require('express');
 const UserRouter = express.Router();
 
-const {addToCart} = require('../controller/user');
+const {clearCart,addToCart} = require('../controller/user');
+
 
 UserRouter.route('/add-to-cart').post(addToCart);
-
+UserRouter.route('/clear-cart').post(clearCart);
 export default UserRouter;
